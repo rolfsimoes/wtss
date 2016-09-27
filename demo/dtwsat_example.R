48501e5058eefa71cc75dd786c62371bc8fd0ec6
luizassis <luizffga@dpi.inpe.br>
2016-09-27 09:52:42 -0300

    new dtwsat demo example

--- demo/dtwsat_example.R
+++ demo/dtwsat_example.R
@@ -1,32 +1,44 @@
+#-----------------------------------------------------------------
+#--                                                             --
+#--   (c) Adeline Maciel <adeline.maciel@inpe.br>               --
+#--       Luiz Fernando Assis <luizffga@dpi.inpe.br>            --
+#--                                                             --
+#--       Image Processing Division                             --
+#--       National Institute for Space Research (INPE), Brazil  --
+#--                                                             --
+#--                                                             --
+#--   dtwSat with wtss.R example - 2016-09-26                   --
+#--                                                             --
+#-----------------------------------------------------------------
+
 # installing and loading packages
 library(dtwSat)
 library(wtss.R)
 
-# create a connection using a serverUrl
+# create a connection using a WTSS class
 server = WTSS("http://www.dpi.inpe.br/tws/wtss")
 
 # get the list of coverages provided by the service
 coverages = listCoverages(server)
 
-# get the description of the third coverage
+# get the description of the second coverage
 cv = describeCoverage(server, coverages[2])
 
 # define attributes list
 attr <- c("ndvi", "evi",  "red",  "nir",  "blue", "mir" )
 
 # get a time series
-spatio_temporal = timeSeries(server, names(cv), attributes=attr, latitude=-10.408, longitude=-53.495, start="2000-02-18", end="2016-01-01")
+spatio_temporal = timeSeries(server, names(cv), attributes=attr, latitude=-11.62399, longitude=-56.2397, start="2000-02-18", end="2016-01-01")
 
-# plot the time-series in a zoo object
-ts = twdtwTimeSeries(spatio_temporal[[names(spatio_temporal)]]$attributes)
-plot(ts, type="timeseries")
+# put ts and signatures into the same scale
+spatio_temporal_ts1 <- spatio_temporal[[names(spatio_temporal)]]$attributes*0.0001
 
-# in the region where the time series was observed we have soybean, cotton, and maize, whose typical temporal pattern
-patt = twdtwTimeSeries(patterns.list)
+# transform time series into twdtwTimeSeries
+ts = twdtwTimeSeries(spatio_temporal_ts1)
 
-# using the the temporal patterns to run the TWDTW analysis
-log_fun = logisticWeight(alpha=-0.1, beta=100) # Logistic time-weight
-matches = twdtwApply(x=ts, y=patt, weight.fun=log_fun, keep=TRUE) 
+# applying TWDTW analysis:
+log_fun = logisticWeight(alpha=-0.1, beta=50) # Logistic time-weight
+matches = twdtwApply(x=ts, y=yearly_patterns_mt, weight.fun=log_fun, keep=TRUE) 
 
 # plot the alignments
 plot(x = matches, type = "alignments")
