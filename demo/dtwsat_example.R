662d83bf1d0c1cb313c0154d10856ddd7a8c552d
luizassis <luizffga@dpi.inpe.br>
2016-09-30 14:16:54 -0300

    update dtwsat demo

--- demo/dtwsat_example.R
+++ demo/dtwsat_example.R
@@ -15,6 +15,8 @@
 library(dtwSat)
 library(wtss.R)
 
+############################ Manipulate the time series
+
 # create a connection using a WTSS class
 server = WTSS("http://www.dpi.inpe.br/tws/wtss")
 
@@ -25,7 +27,7 @@ coverages = listCoverages(server)
 cv = describeCoverage(server, coverages[2])
 
 # define attributes list
-attr <- c("ndvi", "evi",  "red",  "nir",  "blue", "mir" )
+attr <- c("ndvi", "evi")
 
 # get a time series
 spatio_temporal = timeSeries(server, names(cv), attributes=attr, latitude=-11.62399, longitude=-56.2397, start="2000-02-18", end="2016-01-01")
@@ -36,9 +38,26 @@ spatio_temporal_ts1 <- spatio_temporal[[names(spatio_temporal)]]$attributes*0.00
 # transform time series into twdtwTimeSeries
 ts = twdtwTimeSeries(spatio_temporal_ts1)
 
-# applying TWDTW analysis:
-log_fun = logisticWeight(alpha=-0.1, beta=50) # Logistic time-weight
-matches = twdtwApply(x=ts, y=yearly_patterns_mt, weight.fun=log_fun, keep=TRUE) 
+############################ Manipulate the patterns
+
+# building patterns according to the attributes
+patterns <- lapply(yearly_patterns_mt@timeseries, function(p) {
+  object_zoo <- vector("list")
+  dates <- index(p)
+  for(j in 1:length(attr))
+    object_zoo[[j]] <- as.vector(coredata(p[,c(j)]))
+  names(object_zoo) <- attr
+  as.zoo(as.data.frame(object_zoo), dates)
+})
+
+names(patterns) <- names(yearly_patterns_mt@timeseries)
+
+# transforming patterns into twdtw Time Series
+patt = twdtwTimeSeries(patterns)
+
+# Applying TWDTW Analysis
+log_fun = logisticWeight(alpha=-0.1, beta=100) 
+matches = twdtwApply(x=ts, y=patt, weight.fun=log_fun, keep=TRUE)  
 
 # plot the alignments
-plot(x = matches, type = "alignments")
+plot(x = matches, type = "classification", overlap=0.5)
